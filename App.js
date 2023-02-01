f75c1df06476da87bd926f4ef4a50c0127d22a48
DMarjar <20213tn119@utez.edu.mx>
2023-02-01 09:39:50 -0600

    Add Profile & About in Navigation
    Se creo Profile y About en el Navigation, ademas se pusieron iconos que cambian de color.

--- App.js
+++ App.js
@@ -4,41 +4,11 @@ import { Button } from "@rneui/themed";
 import { initializeApp } from "firebase/app";
 import Login from "./modules/auth/adapters/components/Login";
 import React from "react";
+import Navigation from "./config/navigation/Navigation";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Login />
-      <Button
-        title="Maniac Button"
-        buttonStyle={{
-          backgroundColor: "rgba(78, 116, 289, 1)",
-          borderRadius: 3,
-        }}
-        containerStyle={{
-          width: 200,
-          marginHorizontal: 50,
-          marginVertical: 10,
-        }}
-      />
-      
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Navigation />;
 }
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyABpHgw1zHJYFOWGWrhHTsxlAPmhBF-G2E",
-  authDomain: "financer-31c28.firebaseapp.com",
-  projectId: "financer-31c28",
-  storageBucket: "financer-31c28.appspot.com",
-  messagingSenderId: "594821396842",
-  appId: "1:594821396842:web:2298893ad3e227595f8d13"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 const styles = StyleSheet.create({
   container: {
@@ -48,3 +18,16 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyABpHgw1zHJYFOWGWrhHTsxlAPmhBF-G2E",
+  authDomain: "financer-31c28.firebaseapp.com",
+  projectId: "financer-31c28",
+  storageBucket: "financer-31c28.appspot.com",
+  messagingSenderId: "594821396842",
+  appId: "1:594821396842:web:2298893ad3e227595f8d13",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- config/navigation/Navigation.js
+++ config/navigation/Navigation.js
@@ -1,3 +1,54 @@
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from "@rneui/base";
+import Profile from "../../modules/profile/adapters/screens/Profile";
+import About from "../../modules/about/adapters/screens/About";
 
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createBottomTabNavigator();
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="profile" // Muestra por defecto el componente Profile
+        screenOptions={({ route }) => ({ // Opciones de los tabs (iconos, colores, etc)
+          tabBarIcon: ({ color }) => screenOptions(route, color),
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen
+          name="profile"
+          options={{ title: "Perfil" }} // Titulo que se muestra en el tab
+          component={Profile} // Componente que se renderiza
+        />
+        <Tab.Screen
+          name="about"
+          options={{ title: "Acerca de" }} // Titulo que se muestra en el tab
+          component={About} // Componente que se renderiza
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const screenOptions = (route, color) => {
+  let iconName;
+  switch (route.name) {
+    case "profile":
+      iconName = "account";
+      break;
+    case "about":
+      iconName = "information";
+      break;
+  }
+  return (
+    <Icon
+      type="material-community"
+      name={iconName}
+      size={22} // Tamaño del icono en pixeles
+      color={color} // Color del icono
+    />
+  );
+};
